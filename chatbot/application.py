747df94a240efc2ecc3c5a098a8c0ad68aa96893
garden-jun <jeongwonjun0718@gmail.com>
2023-05-27 23:46:05 +0900

    feat: Developed KakaoTalk response API

--- chatbot/application.py
+++ chatbot/application.py
@@ -1,12 +1,41 @@
-from flask import Flask
-import sys
-application = Flask(__name__)
+import test
+from flask import Flask, request, jsonify
 
+
+application = Flask(__name__)
 
 @application.route("/")
 def hello():
     return "Hello goorm!"
 
+@application.route("/music",methods=['POST'])
+def music():
+    req = request.get_json()
+    
+    
+    content = req['userRequest']['utterance']
+    print(content)
+
+    music = test.test(content)
+    
+    print(music)
+    
+    # 답변 텍스트 설정
+    res = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": music
+                    }
+                }
+            ]
+        }
+    }
+
+    # 답변 전송
+    return jsonify(res)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', port=5000, threaded=True)
